73b3b8431e0552b2e5b20ebbfddf965d12b2b260
IrjiSyahrul <161569936+IrjiSyahrul@users.noreply.github.com>
2024-04-04 11:37:01 +0700

    json 1

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace modul7
+{
+    public static void Main()
+    {
+        WriteJSON();
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
